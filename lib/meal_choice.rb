00c69c934e185969a8184fcb419235ae1b160002
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-02 05:44:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,7 +6,9 @@ def meal_choice(veg1, veg2, protein = tofu)
   
   puts "What a nutritious meal!"
   
-  puts "A plate of #{protein} with #{veg"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+  
 
 
 end 
